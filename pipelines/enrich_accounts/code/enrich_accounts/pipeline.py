c5ed72320b57cd08f90f0b43754404796b751739
suman.shiwam@prophecy.io <suman.shiwam@prophecy.io>
2026-06-01 16:19:51 +0000

    Add new `enrich_accounts` pipeline with configuration, UDFs, and test scaffolding

---
+++ pipelines/enrich_accounts/code/enrich_accounts/pipeline.py
@@ -0,0 +1,22 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from enrich_accounts.config.ConfigStore import *
+from enrich_accounts.functions import *
+from prophecy.utils import *
+
+def pipeline(spark: SparkSession) -> None:
+    pass
+
+def main():
+    spark = SparkSession.builder.enableHiveSupport().appName("enrich_accounts").getOrCreate()
+    Utils.initializeFromArgs(spark, parse_args())
+    spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/enrich_accounts")
+    spark.conf.set("spark.default.parallelism", "4")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    registerUDFs(spark)
+    
+    MetricsCollector.instrument(spark = spark, pipelineId = "pipelines/enrich_accounts", config = Config)(pipeline)
+
+if __name__ == "__main__":
+    main()
